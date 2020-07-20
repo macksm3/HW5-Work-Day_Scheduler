b1499402c20642b257a58f82aaab881af9d37593
MacksM3 <64023042+macksm3@users.noreply.github.com>
2020-07-20 16:00:30 -0400

    html framework

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # HW5-Work-Day_Scheduler
+
+using the provided css file as guide, I created html to match layout in provided asset image.
+
+
---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+// HW5 Work Day Scheduler js File
+// document ready 
+$(function() {
+  // day/date at top of page 
+  $("#currentDay").text(moment().format("dddd, MMMM Do"));
+
+
+});
+
